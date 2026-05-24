bbd21035b77bfd50c109c256517667470643a015
Soheil <iamsoheil.r@gmail.com>
2026-05-25 00:18:43 +0330

    Setup linting(ruff+djlint) and apply codebase reformatting

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
+
 from . import views
 
 # Best practice is to have a separate urls.py file for each app and then connect it to the main urls.py file of the project
 
-urlpatterns = [
-    path('',views.index),
-    path('item/', views.item)
-]
+urlpatterns = [path("", views.index), path("item/", views.item)]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+
 
 # HTTP response can return html as well
 def item(request):
