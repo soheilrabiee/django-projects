d56522646f62ce9a41dcb5c3c7b48afcf1423736
Soheil <iamsoheil.r@gmail.com>
2026-05-24 22:37:11 +0330

    Add another view to myapp

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+
+# HTTP response can return html as well
+def item(request):
+    return HttpResponse("<h1>This is an item view</h1>")
