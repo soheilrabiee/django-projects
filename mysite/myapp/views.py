a59f861ae47cfd4e58f313282636a6dab184e8a5
Soheil <iamsoheil.r@gmail.com>
2026-05-28 18:05:41 +0330

    Retrieve data from Item table and pass it as a response

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 
+from .models import Item
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    # Model.Manager.Method => how to retrieve data from the database
+    item_list = Item.objects.all()
+
+    return HttpResponse(item_list)
 
 
 # HTTP response can return html as well
