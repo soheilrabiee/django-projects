c7c2b2acd3ed062a5596369b48cf7b0c41febb95
Soheil <iamsoheil.r@gmail.com>
2026-07-31 22:12:55 +0330

    Add image field to the items

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -10,3 +10,6 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500, default="https://alcaratello.com/wp-content/uploads/2021/03/meal-placeholder.jpg"
+    )
