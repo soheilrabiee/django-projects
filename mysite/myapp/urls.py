b0db6edb0fb271e570c7b46c8f3c41554adc7a08
Soheil <iamsoheil.r@gmail.com>
2026-07-31 19:13:14 +0330

    Using dynamic url with the url tag in detail page

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -5,8 +5,8 @@ from . import views
 # Best practice is to have a separate urls.py file for each app and then connect it to the main urls.py file of the project
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("item/", views.item),
     # Catching id value from the dynamic url
-    path("<int:id>/", views.detail),
+    path("<int:id>/", views.detail, name="detail"),
 ]
