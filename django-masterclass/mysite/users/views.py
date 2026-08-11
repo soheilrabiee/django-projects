8eae74874e853416cd0e0e8c7783be88c873cc69
Soheil <iamsoheil.r@gmail.com>
2026-08-12 02:09:28 +0330

    Show a success message to the user after the signup

--- django-masterclass/mysite/users/views.py
+++ django-masterclass/mysite/users/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+
 # Django automatically handles the user creation form and model
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
@@ -11,6 +13,11 @@ def register(request):
     if request.method == "POST":
         if form.is_valid():
             form.save()
+            # Cleaned data is the data after validation
+            username = form.cleaned_data.get("username")
+            # Save a success message for the given request
+            # The message survives the redirect
+            messages.success(request, f"Welcome {username}, your account has been successfully created")
             return redirect("myapp:index")
 
     return render(request, "users/register.html", {"form": form})
