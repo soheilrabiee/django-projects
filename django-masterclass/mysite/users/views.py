bed1d07dd9665d4aa66370d4a2ef2fb823fe3422
Soheil <iamsoheil.r@gmail.com>
2026-08-11 23:13:15 +0330

    Handle the post request of the registration form and save the data into the database

--- django-masterclass/mysite/users/views.py
+++ django-masterclass/mysite/users/views.py
@@ -1,10 +1,16 @@
 # Django automatically handles the user creation form and model
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect("myapp:index")
+
     return render(request, "users/register.html", {"form": form})
