c65682b83e6aa197717eb9b6ea6e74efe9101936
Soheil <iamsoheil.r@gmail.com>
2026-08-13 22:42:21 +0330

    Add login template and connect it to the view

--- django-masterclass/mysite/users/urls.py
+++ django-masterclass/mysite/users/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="register"),
     # The as_view method needs to be called whenever a class based view is used
-    path("login/", auth_views.LoginView.as_view(), name="login"),
+    # The template_name shows what template needs to be rendered
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
 ]
