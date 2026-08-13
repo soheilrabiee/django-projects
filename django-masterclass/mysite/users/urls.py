a76f779efa7c5bd2a92fa718d8ca406511cb061a
Soheil <iamsoheil.r@gmail.com>
2026-08-13 21:59:18 +0330

    Add django built-in login view to the urls file

--- django-masterclass/mysite/users/urls.py
+++ django-masterclass/mysite/users/urls.py
@@ -1,5 +1,11 @@
+# Django has a built-in view ready to use for login
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
 
-urlpatterns = [path("register/", views.register, name="register")]
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    # The as_view method needs to be called whenever a class based view is used
+    path("login/", auth_views.LoginView.as_view(), name="login"),
+]
