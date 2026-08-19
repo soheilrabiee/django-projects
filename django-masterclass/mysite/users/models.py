0ef4386dc5a08f4b7b964440ad2e5ca209943db7
Soheil <iamsoheil.r@gmail.com>
2026-08-19 23:25:15 +0330

    Create the profile model and create a relationship to the django's user model

--- django-masterclass/mysite/users/models.py
+++ django-masterclass/mysite/users/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    # Creating a 1 to 1 relationship between the profile model and the user model
+    # The CASCADE makes sure that whenever an object is deleted in the user model the same profile for that object in the profile model gets deleted as well
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="profilepic.jpg", upload_to="profile_pictures")
+    location = models.CharField(max_length=100)
+
+    # Because of the relationship between the user and profile model the data in the user model can be accessed in the profile model
+    def __str__(self):
+        return self.user.username
