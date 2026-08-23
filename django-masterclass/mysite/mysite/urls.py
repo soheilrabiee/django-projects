542cdc7ae31c87501664e2c63c032fc43c86ed9d
Soheil <iamsoheil.r@gmail.com>
2026-08-23 18:28:33 +0330

    Add static url for the media images

--- django-masterclass/mysite/mysite/urls.py
+++ django-masterclass/mysite/mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -23,4 +25,5 @@ urlpatterns = [
     # Connecting myapp urls to the project base urls
     path("myapp/", include("myapp.urls")),
     path("users/", include("users.urls")),
-]
+    # Serve user-uploaded media files from MEDIA_ROOT during DEVELOPMENT
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
