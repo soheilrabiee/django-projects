e5f89a7588814341fcde6881eaf179907cb345da
Soheil <iamsoheil.r@gmail.com>
2026-09-26 02:52:56 +0330

    Add new order model to the app and register it for the admin

--- django-masterclass/mysite/myapp/admin.py
+++ django-masterclass/mysite/myapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Item
+from .models import Item, Order
 
 # Register your models here.
 # For the admin to have access to it
 admin.site.register(Item)
+admin.site.register(Order)
