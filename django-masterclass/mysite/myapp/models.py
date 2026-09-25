5124d59b21239357a0b1f5fd936d95854eed690b
Soheil <iamsoheil.r@gmail.com>
2026-09-26 02:27:14 +0330

    Alter item_image field from URL to image in the item model

--- django-masterclass/mysite/myapp/models.py
+++ django-masterclass/mysite/myapp/models.py
@@ -34,9 +34,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200, db_index=True)
     item_desc = models.CharField()
     item_price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
-    item_image = models.URLField(
-        max_length=500, default="https://alcaratello.com/wp-content/uploads/2021/03/meal-placeholder.jpg"
-    )
+    # item_image = models.URLField(
+    #     max_length=500, default="https://alcaratello.com/wp-content/uploads/2021/03/meal-placeholder.jpg"
+    # )
+    item_image = models.ImageField(upload_to="item_images/", blank=True, null=True)
     is_available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
