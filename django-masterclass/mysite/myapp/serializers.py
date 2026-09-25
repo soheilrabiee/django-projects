8c258058ae1153148c26e95d102ae6e959dbefc4
Soheil <iamsoheil.r@gmail.com>
2026-09-25 18:28:34 +0330

    Display the username string representation for each item in ItemSerializer

--- django-masterclass/mysite/myapp/serializers.py
+++ django-masterclass/mysite/myapp/serializers.py
@@ -4,8 +4,10 @@ from .models import Item
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    user_name = serializers.StringRelatedField()
+
     class Meta:
         # Use the Item model for this serializer
         model = Item
         # Fields exposed by the API
-        fields = ["id", "item_name", "item_desc", "item_price", "item_image"]
+        fields = ["id", "user_name", "item_name", "item_desc", "item_price", "item_image"]
